5e67018672e3b671b8f322d0afb9bf742fef5dbf
krishnaCG-1997 <krishna.a.agarwal@capgemini.com>
2021-09-28 17:24:01 +0530

    image path changed in login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  multimediaPrefix : "https://powerboard-test.s3.eu-central-1.amazonaws.com/uploads/uploads/multimedia/",
+  logoPrefix : "https://powerboard-test.s3.eu-central-1.amazonaws.com/uploads/uploads/logo/",
+  localPrefix : "assets/uploads/multimedia/",
+  slideshowInterval: 3000
 };
